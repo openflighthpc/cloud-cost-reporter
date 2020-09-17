8e45170319554ba98bf82ff5d7cee9dd2151e7fb
VoxSecundus <millard64@hotmail.co.uk>
2020-09-17 17:53:04 +0100

    Add basic, initial costs query for 2 days ago

--- models/azure_project.rb
+++ models/azure_project.rb
@@ -28,6 +28,61 @@ class AzureProject < Project
     @metadata['bearer_expiry']
   end
 
+  def get_cost_and_usage(date=Date.today-2)
+    # refresh authorization token if necessary
+    # tokens last for 3600 seconds (one hour)
+    if Time.now.to_i > bearer_expiry.to_i
+      update_bearer_token
+    end
+
+    cost_log = cost_logs.find_by(date: date.to_s)
+
+    if !cost_log
+      daily_cost = api_query_daily_cost(date)
+
+      cost_log = CostLog.create(
+        project_id: id,
+        cost: daily_cost,
+        currency: 'GBP',
+        date: date.to_s,
+        timestamp: Time.now.to_s
+      )
+
+      msg = "
+        :moneybag: Usage for #{(Date.today - 2).to_s} :moneybag:
+        *GBP:* #{cost_log.cost.to_f.ceil(2)}
+        *Compute Units (Flat):* #{cost_log.compute_cost}
+        *Compute Units (Risk):* #{cost_log.risk_cost}
+        *FC Credits:* #{cost_log.fc_credits_cost}
+      "
+
+      send_slack_message(msg)
+    end
+  end
+
+  def api_query_daily_cost(date)
+    uri = "https://management.azure.com/subscriptions/#{subscription_id}/providers/Microsoft.Consumption/usageDetails"
+    query = {
+      'api-version': '2019-10-01',
+      '$filter': "properties/usageStart eq '#{date.to_s}' and properties/usageEnd eq '#{date.to_s}'"
+    }
+    response = HTTParty.get(
+      uri,
+      query: query,
+      headers: { 'Authorization': "Bearer #{bearer_token}" }
+    )
+
+    if response.success?
+      charges = response['value']
+      # the query has multiple values that sound useful (effectivePrice, cost, 
+      # quantity, unitPrice). 'cost' is the value that is used on the Azure Portal
+      # Cost Analysis page (under 'Actual Cost') for the period selected.
+      return charges.map { |c| c['properties']['cost'] }.reduce(:+)
+    else
+      raise RuntimeError, "Error querying Azure API. Error code #{response.code}."
+    end
+  end
+
   def update_bearer_token
     response = HTTParty.post(
       "https://login.microsoftonline.com/#{tenant_id}/oauth2/token",
