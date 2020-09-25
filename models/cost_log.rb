5a1c667f5208fb043d96da97541d6347ba603861
timalces <tim.brackfield@alces-software.com>
2020-09-25 16:52:46 +0100

    added currency conversion and file run examples

--- models/cost_log.rb
+++ models/cost_log.rb
@@ -5,12 +5,17 @@ ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/cost_t
 class CostLog < ActiveRecord::Base
   belongs_to :project
 
+  def usd_to_gbp
+    ENV["USD_CONVERSION"] ? ENV["USD_CONVERSION"].to_f : 0.77
+  end
+
   def compute_cost
-    (self.cost.to_f * 10).ceil
+    gbp_cost = self.currency == "USD" ? (self.cost.to_f * usd_to_gbp) : self.cost.to_f
+    (gbp_cost * 10).ceil
   end
 
   def risk_cost
-    (compute_cost * 1.25).ceil
+    (compute_cost.to_f * 1.25).ceil
   end
 
   def fc_credits_cost
