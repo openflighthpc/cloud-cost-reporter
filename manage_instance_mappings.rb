cf4ccb06b6a5c3ab0a2c10f16fe38812a962cb1c
timalces <tim.brackfield@alces-software.com>
2020-09-25 12:16:19 +0100

    no recursion

--- manage_instance_mappings.rb
+++ manage_instance_mappings.rb
@@ -1,61 +1,68 @@
 require_relative './models/instance_mapping'
 
-def add_or_update_mapping(action=nil)
-  if action == nil
+def add_or_update_mapping
+  valid_action = false
+  while !valid_action
     print "Add, update or delete customer facing instance name (add/update/delete)? "
     action = gets.chomp.downcase
-  end
-  if action == "update" || action == "delete"
-    print "Instance type name: "
-    type_name = gets.chomp
-    mapping = InstanceMapping.find_by(instance_type: type_name)
-    if mapping == nil
-      puts "Mapping for that instance type not found. Please try again."
-      return add_or_update_mapping(action)
+    if action == "update" || action == "delete"
+      valid_action = true
+      mapping = nil
+      while !mapping
+        print "Instance type name: "
+        type_name = gets.chomp
+        mapping = InstanceMapping.find_by(instance_type: type_name)
+        if mapping == nil
+          puts "Mapping for that instance type not found. Please try again."
+        end
+      end
+      puts "Customer facing name: #{mapping.customer_facing_name}"
+      action == "update" ? update_attributes(mapping) : delete_mapping(mapping)
+    elsif action == "add"
+      valid_action = true
+      add_mapping
+    else
+      puts "Invalid selection, please try again."
     end
-    puts "Customer facing name: #{mapping.customer_facing_name}"
-    action == "update" ? update_attributes(mapping) : delete_mapping(mapping)
-  elsif action == "add"
-    add_mapping
-  else
-    puts "Invalid selection, please try again."
-    add_or_update_mapping
   end
 end
 
 def update_attributes(mapping)
-  valid = false
-  attribute = nil
-  while !valid
-    puts "What would you like to update (type/name)? "
-    attribute = gets.chomp
-    if ["type", "name"].include?(attribute)
-      valid = true
-    else
-      "That is not a valid attribute. Please try again."
+  stop = false
+  while !stop
+    valid = false
+    attribute = nil
+    while !valid
+      puts "What would you like to update (type/name)? "
+      attribute = gets.chomp
+      if ["type", "name"].include?(attribute)
+        valid = true
+      else
+        puts "That is not a valid attribute. Please try again."
+      end
     end
-  end
 
-  attribute = attribute == "type" ? :instance_type : :customer_facing_name
-  print 'Value: '
-  value = gets.chomp
-  mapping.write_attribute(attribute, value)
-  valid = mapping.valid?
-  while !valid
-    mapping.errors.messages.each do |k, v|
-      puts "#{k} #{v.join("; ")}"
-      puts "Please enter new #{k}"
-      value = gets.chomp
-      mapping.write_attribute(k, value)
-    end
+    attribute = attribute == "type" ? :instance_type : :customer_facing_name
+    print 'Value: '
+    value = gets.chomp
+    mapping.write_attribute(attribute, value)
     valid = mapping.valid?
-  end
-  mapping.save!
-  puts "#{attribute} updated successfully"
-  print "Would you like to update another field (y/n)? "
-  action = gets.chomp.downcase
-  if action == "y"
-    return update_attributes(mapping)
+    while !valid
+      mapping.errors.messages.each do |k, v|
+        puts "#{k} #{v.join("; ")}"
+        puts "Please enter new #{k}"
+        value = gets.chomp
+        mapping.write_attribute(k, value)
+      end
+      valid = mapping.valid?
+    end
+    mapping.save!
+    puts "#{attribute} updated successfully"
+    print "Would you like to update another field (y/n)? "
+    action = gets.chomp.downcase
+    if action == "n"
+      stop = true
+    end
   end
 end
 
